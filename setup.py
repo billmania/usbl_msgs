5baa2437effa9c0466bb35fadf778555ac003108
Bill Mania <bill@manialabs.us>
2023-03-20 13:41:50 -0700

    Defined the RangeBearing message, in its own ROS2 package.

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+from glob import glob
+
+package_name = 'usbl_msgs'
+
+setup(
+    name=package_name,
+    version='0.0.2',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Bill Mania',
+    maintainer_email='bill@manialabs.us',
+    description='USBL message definitions',
+    license='Proprietary',
+    },
+)
